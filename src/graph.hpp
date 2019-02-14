117e5d3238eb953d647766ca9a4d2547bde38a25
Wajih Ouertani <wajih.ouertani@gmail.com>
2019-02-15 00:52:58 +0100

    update dijkstra impl. with std::priority_queue + random graph constructor with density + update methods

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,7 +1,11 @@
+/**
+ * @author Wajih Ouertani
+ * @email wajih.ouertani@gmail.com
+ */
+
 #ifndef MYLIB_GRAPH
 #define MYLIB_GRAPH
 
-#include "edge.hpp"
 #include <functional>
 #include <iostream>
 #include <unordered_map>
@@ -10,8 +14,11 @@
 #include <stdexcept>
 #include <utility>
 #include <vector>
+#include <queue>
 
 #include <boost/optional.hpp>
+#include "utils.hpp"
+#include "edge.hpp"
 
 namespace mylib {
 
@@ -23,15 +30,66 @@ protected:
 
 public:
   static constexpr const char *INFO = "mylib::Graph";
+
+
+  /**
+   * Constructors
+   */
+  Graph() {}
+  explicit Graph(Graph<Node> const &g_source) { graph = g_source.graph; }
+  explicit Graph(Graph<Node> *const g_source) { graph = g_source->graph; }
+  /**
+   * Construct a collapsed (d(x,y)==0 for all x, y in graph nodes) complete
+   * graph from a list of nodes
+   */
+  explicit Graph(std::vector<Node> nodes) {
+    for (auto end_1 : nodes) {
+      for (auto end_2 : nodes) {
+        add_edge(new Edge<Node>(end_1, end_2));
+      }
+    }
+  }
+
+  /** a Constructor using Node vector a generator of edges as a function of (Node from_, Node to_)
+   */
+  explicit Graph(std::vector<Node> nodes,
+                   std::function<Edge<Node>(Node, Node)> generator, double density=1) {
+    for (auto from_ : nodes) {
+      for (auto to_ : nodes) {
+        // generate edge accordingly with density parameter
+        if (utils::random<double>(0, 1) <= density) {
+          add_edge(generator(from_, to_));
+        }
+      }
+    }
+  }
+
   /**
   return size or more precisely the number of edges
   */
-  unsigned int get_size() const {
-    unsigned int size;
-    for (auto it = graph.begin(); it < graph.end(); it++) {
+  unsigned int get_edges_number() const {
+    unsigned int size=0;
+    for (auto it=graph.begin(); it != graph.end(); it++) {
       size += it->second.size();
     }
     return size;
+  }
+  unsigned int E() const {
+    return this->get_edges_number();
+  }
+
+  /** return vertices/nodes number
+   */
+
+  unsigned int get_nodes_number() const {
+    std::set<Node> nodes;
+    for (auto it=graph.begin(); it != graph.end(); it++) {
+      nodes.insert(it->first);
+      for (auto node:it->second) {
+        nodes.insert(node.first);
+      }
+    }
+    return nodes.size();
   }
 
   /*
@@ -54,6 +112,35 @@ public:
     }
     return {};
   }
+
+  /**
+   * return edge value if the edge exists
+   */
+
+  boost::optional<double> get_edge_value(Node from_, Node to_) {
+    auto edge = get_edge(from_, to_);
+    if (edge) {
+      return edge.get().get_distance(); // TODO refactor get_distance to get_value (more generic)
+    }
+    return {};
+  }
+
+  /**
+   * set edge value (if edge exists/already created)
+   * does not create the edge if  does not exist
+  */
+  boost::optional<bool> set_edge_value(Node from_, Node to_, double value) {
+
+  auto source_edges = graph.find(from_);
+    if (source_edges != graph.end()) {
+      auto source_dest_edge = source_edges->second.find(to_);
+      if (source_dest_edge != source_edges->second.end()) {
+        return {source_dest_edge->second.set_value(value)};
+      }
+    }
+    return {};
+  }
+
 
   void add_edge(Edge<Node> edge) {
     // check the first end
@@ -81,33 +168,6 @@ public:
   }
 
   /**
-   * Constructors
-   */
-  Graph() {}
-  explicit Graph(Graph<Node> const &g_source) { graph = g_source.graph; }
-  explicit Graph(Graph<Node> *const g_source) { graph = g_source->graph; }
-  /**
-   * Construct a collapsed (d(x,y)==0 for all x, y in graph nodes) complete
-   * graph from a list of nodes
-   */
-  explicit Graph(std::vector<Node> nodes) {
-    for (auto end_1 : nodes) {
-      for (auto end_2 : nodes) {
-        add_edge(new Edge<Node>(end_1, end_2));
-      }
-    }
-  }
-
-  explicit Graph(std::vector<Node> nodes,
-                   std::function<Edge<Node>(Node, Node)> generator) {
-    for (auto from_ : nodes) {
-      for (auto to_ : nodes) {
-        add_edge(generator(from_, to_));
-      }
-    }
-  }
-
-  /**
    * filter the graph using a filtering function (functional style)
    */
   Graph *filter(std::function<bool(Edge<Node>)> filter) {
@@ -126,19 +186,11 @@ public:
    * return directly reachable nodes (with defined direct edge)
    */
   boost::optional<std::unordered_map<Node, Edge<Node>>>
-  get_next_reachable(Node source) {
-
+  neighbors(Node source) {
     auto connected_nodes = graph.find(source);
     if (connected_nodes != graph.end()) {
-      // debug print
-      std::cout << "source: " << source;
-      for (auto node : connected_nodes->second) {
-        std::cout << " -> " << node.first << " (" << node.second << ")";
-      }
-      std::cout << std::endl;
       return connected_nodes->second;
     }
-    std::cout << "node is not connected" << std::endl; //  TODO: proper logging
     return {};
   }
 
@@ -204,33 +256,39 @@ public:
   std::vector<Node> dijkstra(Node starting_node_index, Node final_node_index) {
     std::set<Node> explored(
         {starting_node_index}); // list of explored nodes (closed set)
-    std::vector<std::vector<Node>> paths(
-        {{starting_node_index}}); // paths to explore (extend)
+
+    auto compare = [] (std::pair<std::vector<Node>, double> left, std::pair<std::vector<Node>, double> right) {
+      return left.second > right.second;
+    };
+
+    std::priority_queue<std::pair<std::vector<Node>, double>, std::vector<std::pair<std::vector<Node>, double>>,
+                      decltype(compare)> paths(compare);
+    paths.push(std::make_pair<std::vector<Node>, double>({starting_node_index}, 0.)); // paths to explore (extend)
 
     while (paths.size() > 0) {
-      auto shortest_path_index = get_shortest_path(paths);
-      auto shortest_path = paths[shortest_path_index];
-      paths.erase(paths.begin() +
-                  shortest_path_index); // pop the shortest path to explore
-      if (shortest_path.back() == final_node_index) {
+      auto shortest_path = paths.top();
+      paths.pop();
+
+      if (shortest_path.first.back() == final_node_index) {
         // found shortest path to destination (final node_index)
-        return shortest_path;
+        return shortest_path.first;
       }
-      // get the reachable nodes starting from last node of the path
-      auto next_reachable = get_next_reachable(shortest_path.back());
+      // get the reachable/neighbor nodes starting from last node of the path
+      auto next_reachable = neighbors(shortest_path.first.back());
       if (next_reachable) {
         for (auto node : next_reachable.value()) {
           if (explored.find(node.first) == explored.end()) {
             // extend the path with the node (as the node is not yet in the
             // explored set)
-            auto path_to_extend = shortest_path;
+            auto path_to_extend = shortest_path.first;
             path_to_extend.push_back(node.first);
-            paths.push_back(path_to_extend);
+            // update the paths priority queue
+            paths.push(std::make_pair(path_to_extend, get_path_length(path_to_extend)));
           }
         }
       }
       // update the explored set
-      explored.insert(shortest_path.back());
+      explored.insert(shortest_path.first.back());
     }
     return {};
   }
