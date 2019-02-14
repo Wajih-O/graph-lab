bb290fd76613a0d8bcf36fca767cca940b2b3cf6
Wajih Ouertani <wajih.ouertani@gmail.com>
2019-02-15 00:55:04 +0100

    update graph tests

--- tests/test_graph.hpp
+++ tests/test_graph.hpp
@@ -5,14 +5,13 @@
 
 
 TEST(Graph, should_return_direct_reachable_nodes) {
-    
     mylib::Graph<int> graph;
     graph.add_edge(mylib::Edge<int>(1, 0, 50.));
     graph.add_edge(mylib::Edge<int>(1, 2, 17.));
     graph.add_edge(mylib::Edge<int>(0, 1, 10.));
 
     std::set<std::pair<int, double>> nodes;
-    auto next = graph.get_next_reachable(1);
+    auto next = graph.neighbors(1);
     if (next) {
         for (auto node: next.get()) {
             nodes.insert(std::make_pair(node.first, node.second.get_distance()));
@@ -22,7 +21,7 @@ TEST(Graph, should_return_direct_reachable_nodes) {
 }
 
 
-/** 
+/**
  * Test path length of std::vector<Node>
  */
 
@@ -31,11 +30,11 @@ TEST(Graph, should_return_path_length) {
     graph.add_edge(mylib::Edge<int>(1, 0, 50.));
     graph.add_edge(mylib::Edge<int>(1, 2, 17.));
     graph.add_edge(mylib::Edge<int>(0, 1, 10.));
-    
+
     ASSERT_EQ(graph.get_path_length({0, 1, 2}), 27);
 }
 
-/** 
+/**
  * Test path length of std::vector<Node>
  */
 
@@ -44,8 +43,46 @@ TEST(Graph, should_return_correct_path_length_on_no_path) {
     graph.add_edge(mylib::Edge<int>(1, 0, 50.));
     graph.add_edge(mylib::Edge<int>(1, 2, 17.));
     graph.add_edge(mylib::Edge<int>(0, 1, 10.));
-    
+
     ASSERT_EQ(graph.get_path_length({0, 2, 1}), -1);
+}
+
+/**
+ * Test graph edges number (E)
+ */
+
+TEST(Graph, should_return_correct_edges_number) {
+    mylib::Graph<int> graph;
+    graph.add_edge(mylib::Edge<int>(1, 0, 50.));
+    graph.add_edge(mylib::Edge<int>(1, 2, 17.));
+    graph.add_edge(mylib::Edge<int>(0, 1, 10.));
+
+    ASSERT_EQ(graph.E(), 3);
+}
+
+/**
+ * Test graph nodes/vertices number (V)
+ */
+
+TEST(Graph, should_return_correct_vertices_number) {
+    mylib::Graph<int> graph;
+    graph.add_edge(mylib::Edge<int>(1, 0, 50.));
+    graph.add_edge(mylib::Edge<int>(1, 2, 17.));
+    graph.add_edge(mylib::Edge<int>(0, 1, 10.));
+
+    ASSERT_EQ(graph.get_nodes_number(), 3);
+}
+
+
+/**
+ * Test update node
+ */
+
+TEST(Graph, should_update_edge_value) {
+    mylib::Graph<int> graph;
+    graph.add_edge(mylib::Edge<int>(1, 0, 50.));
+    ASSERT_EQ(graph.set_edge_value(1, 0, 3.), true);
+    ASSERT_EQ(graph.get_edge_value(1, 0).get(), 3.);
 }
 
 
@@ -60,7 +97,7 @@ TEST(Graph, should_dijkstra_compute_the_shortest_path) {
   graph.add_symmetric_edge(mylib::Edge<int>(1, 2, 7.));
   graph.add_symmetric_edge(mylib::Edge<int>(1, 3, 9.));
   graph.add_symmetric_edge(mylib::Edge<int>(1, 6, 14.));
-  
+
   graph.add_symmetric_edge(mylib::Edge<int>(2, 1, 7.));
   graph.add_symmetric_edge(mylib::Edge<int>(2, 3, 10.));
   graph.add_symmetric_edge(mylib::Edge<int>(2, 4, 15.));
@@ -75,6 +112,10 @@ TEST(Graph, should_dijkstra_compute_the_shortest_path) {
 
 }
 
+
+// TODO: test random generation of graph given a set of nodes, values range and a density param
+
+
 // TODO: Test shortest path index from a vector of path
 
-// TODO Test edge filter 
+// TODO Test edge filter
