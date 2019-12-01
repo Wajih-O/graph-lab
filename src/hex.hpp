6f1ef038bcf6f4ab14d1e6cf2771db08c1100a1d
Wajih Ouertani <wajih.ouertani@gmail.com>
2019-12-01 19:53:47 +0100

    Hex board player graph connectivity on cell assignment impl. + test

--- src/hex.hpp
+++ src/hex.hpp
@@ -1,4 +1,4 @@
-/* @modify date 2019-11-27 04:14:02
+/* @modify date 2019-12-01 17:04:21
  * @author Wajih Ouertani
  * @email wajih.ouertani@gmail.com
  */
@@ -7,6 +7,7 @@
 #define MYLIB_HEX_BOARD
 
 #include <algorithm>
+#include <set>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -14,17 +15,18 @@
 #include <boost/functional/hash.hpp>
 #include <boost/optional.hpp>
 
-
 #include "graph.hpp"
 #include "utils.hpp"
 
 namespace mylib {
 
-template <class  T> class HexCell {
+template <class T> class HexCell {
 public:
   HexCell(std::size_t i_pos, std::size_t j_pos, T cell_content)
       : i_pos(i_pos), j_pos(j_pos), content(cell_content) {}
-  std::pair<std::size_t, std::size_t> get_position() { return std::make_pair(i_pos, j_pos); }
+  std::pair<std::size_t, std::size_t> get_position() {
+    return std::make_pair(i_pos, j_pos);
+  }
   bool operator==(const HexCell<T> &other) {
     return get_position() == other.get_position();
   }
@@ -33,6 +35,8 @@ protected:
   T content;
   std::size_t i_pos, j_pos;
 };
+
+enum class HEX_DIRECTION { EAST, WEST, NORD, SOUTH, NORD_EAST, SOUTH_WEST };
 
 template <class T> class HexBoard {
 
@@ -44,51 +48,187 @@ template <class T> class HexBoard {
   std::vector<std::vector<T>>
       board; // should be a board of unique pointers to ensure that every cell
              // is assigned to a unique graph
-  std::vector<Graph<std::pair<std::size_t, std::size_t>>> players_graphs; // a graph for each player
-  std::unordered_map<std::pair<std::size_t, std::size_t>, std::size_t, boost::hash<std::pair<std::size_t,std::size_t>> > cell_to_player; // a map assigning each board cell to the player who has collected/marked it
+
+  std::vector<std::unique_ptr<Graph<std::pair<std::size_t, std::size_t>>>>
+      players_graphs; // a graph for each player
+
+  std::unordered_map<std::pair<std::size_t, std::size_t>, std::size_t,
+                     boost::hash<std::pair<std::size_t, std::size_t>>>
+      cell_to_player; // a map assigning each board cell to the player who has
+                      // collected/marked it
   // a graph could be dynamically built from this structure (on demand)
 public:
-  HexBoard(std::size_t x_size, std::size_t y_size, std::size_t players_nbr=2 )
+  HexBoard(std::size_t x_size, std::size_t y_size, std::size_t players_nbr = 2)
       : x_size(x_size), y_size(y_size), players_nbr(players_nbr) {
     board = std::vector<std::vector<T>>(x_size, std::vector<T>(y_size, T()));
-    players_graphs = std::vector<Graph<std::pair<std::size_t, std::size_t>>>(players_nbr, Graph<std::pair<std::size_t, std::size_t>>());
+    // Initializa player graphs
+    players_graphs.reserve(players_nbr);
+    for (int i = 0; i < players_nbr; i++) {
+      players_graphs.emplace_back(
+          new Graph<std::pair<std::size_t, std::size_t>>);
+    }
   }
 
+  // TODO: Refactor the code to connect the nord, south, west, east to special
+  // node to test if a player has connected the nord to south, or the west to
+  // east
 
-  bool within_the_board(std::pair<std::size_t, std::size_t> cell_location) {
-    return (cell_location.first < x_size) && (cell_location.second < y_size);
+  /*
+  * Helper methods to get neighbors of a cell (C) in East (E), west (W), Nord,
+  Nord_East (N-E), South (S), South West (SW)
+  * Note that the grid is rotated (90 degree counter clock-wise) here for ascii
+  drawing convenience,
+
+            /     \       /     \
+      _____/       \_____/       \_____
+     /     \       /     \       /     \
+    /       \_____/   E   \_____/       \
+    \       /     \       /     \       /
+     \_____/  N-E  \_____/   S   \_____/
+     /     \       /     \       /     \
+    /       \_____/   C   \_____/       \
+    \       /     \       /     \       /
+     \_____/   N   \_____/  S-W  \_____/
+     /     \       /     \       /
+    /       \_____/   W   \_____/
+    \       /     \       /
+     \_____/       \_____/
+     /     \       /
+    /       \_____/
+    \       /
+     \_____/
+
+  * returns the next neighbor of a cell for a given direction, if the grid
+  limits are not reached (the neighbor is within the grid)
+  */
+
+  boost::optional<std::pair<std::size_t, std::size_t>>
+  grid_neighbor(std::pair<std::size_t, std::size_t> cell,
+                HEX_DIRECTION direction) {
+    boost::optional<std::pair<std::size_t, std::size_t>> neighbor = {};
+    switch (direction) {
+    case (HEX_DIRECTION::EAST):
+      neighbor = within_the_board(std::make_pair(cell.first, cell.second + 1));
+      break;
+    case (HEX_DIRECTION::WEST):
+      neighbor = within_the_board(std::make_pair(cell.first, cell.second - 1));
+      break;
+    case (HEX_DIRECTION::NORD):
+      neighbor = within_the_board(std::make_pair(cell.first - 1, cell.second));
+      break;
+    case (HEX_DIRECTION::SOUTH):
+      neighbor = within_the_board(std::make_pair(cell.first + 1, cell.second));
+      break;
+    case (HEX_DIRECTION::NORD_EAST):
+      neighbor =
+          within_the_board(std::make_pair(cell.first - 1, cell.second + 1));
+      break;
+    case (HEX_DIRECTION::SOUTH_WEST):
+      neighbor =
+          within_the_board(std::make_pair(cell.first + 1, cell.second - 1));
+      break;
+    }
+    return neighbor;
   }
 
   /*
-  * lookup for a cell int the cell to player map and return player if it exist
-  */
-  boost::optional<std::size_t> get_player(std::pair<std::size_t, std::size_t> cell) {
+   * Return a set of neighbor cell (position/coordinate)
+   * @param cell: (cell of interest position/coordinate as
+   * std::pair<std::size_t, std::size_t>) return: positions within the grid of
+   * the neighbors cells of (cell) as  std::set<std::pair<std::size_t,
+   * std::size_t>>
+   */
+  std::set<std::pair<std::size_t, std::size_t>>
+  all_neighbors(std::pair<std::size_t, std::size_t> cell) {
+    std::set<std::pair<std::size_t, std::size_t>>
+        grid_neighbors; // next neighbors in the grid
+    for (auto direction :
+         {HEX_DIRECTION::EAST, HEX_DIRECTION::WEST, HEX_DIRECTION::NORD,
+          HEX_DIRECTION::SOUTH, HEX_DIRECTION::NORD_EAST,
+          HEX_DIRECTION::SOUTH_WEST}) {
+      auto neighbor = grid_neighbor(cell, direction);
+      if (neighbor) {
+        grid_neighbors.insert(neighbor.get());
+      }
+    }
+    return grid_neighbors;
+  }
+
+  bool is_within_the_board(std::pair<std::size_t, std::size_t> const &cell) {
+    return (cell.first < x_size) && (cell.second < y_size);
+  }
+
+  boost::optional<std::pair<std::size_t, std::size_t>>
+  within_the_board(std::pair<std::size_t, std::size_t> const &cell) {
+    if (is_within_the_board(cell)) {
+      return std::make_pair(cell.first, cell.second);
+    }
+    return {};
+  }
+
+  /*
+   * lookup for a cell int the cell to player map and return player if it exist\
+   * @param cell: a cell location in the grid (cell) as std::pair<std::size_t,
+   * std::size_t>
+   * @return: an optional player index as std::pair<std::size_t, std::size_t>
+   */
+  boost::optional<std::size_t>
+  get_player(std::pair<std::size_t, std::size_t> cell) {
     auto iter = cell_to_player.find(cell);
     if (iter != cell_to_player.end()) {
       return iter->second;
     }
-  return {};
+    return {};
   }
 
-  boost::optional<Graph<std::pair<std::size_t, std::size_t>>> get_player_graph(std::size_t player_index) {
-    if (player_index > players_graphs.size()) return {};
+  /*
+   * lookup for a (unique ptr to) player graph given a player
+   * @param player_index: player index
+   * @return: player graph for the relative player index
+   */
+
+  boost::optional<std::unique_ptr<Graph<std::pair<std::size_t, std::size_t>>> &>
+  get_player_graph(std::size_t player_index) {
+    if (player_index > players_graphs.size())
+      return {};
     return players_graphs[player_index];
   }
 
-  bool assign_cell_to_player(std::pair<std::size_t,std::size_t> cell, std::size_t player_index) {
-    if ((within_the_board(cell)) && (player_index<players_nbr)) {
-      cell_to_player[cell] = player_index;
-      // maintain/build the edges of the player graph after adding the cell
-      auto east_neighbor = std::make_pair(cell.first, cell.second+1);
-      auto west_neighbor = std::make_pair(cell.first, cell.second-1);
+  /*
+   * Assign (allocate) the cell to a given player who played/selected the cell
+   * @param cell: cell to assign to the player
+   * @param player_index: player to whom the cell will be assigned (given)
+   * @return bool: whether the move is valid and finalized (cell assigned to the
+   * player) or not update the player (player_index) with the new edges a
+   * consequence of adding the cell
+   */
 
-      if (within_the_board(west_neighbor) && (get_player(west_neighbor) == player_index)) {
-        players_graphs[player_index].add_edge(mylib::Edge<std::pair<std::size_t,std::size_t>>(west_neighbor, cell, 1));
+  bool assign_cell_to_player(std::pair<std::size_t, std::size_t> cell,
+                             std::size_t player_index) {
+    // Check if the player index is valid, the cell is within the grid and not
+    // already assigned to another player.
+    if ((within_the_board(cell)) && (player_index < players_nbr)) {
+      // check if the cell is assined to another player
+      if (cell_to_player.find(cell) == cell_to_player.end()) {
+        cell_to_player[cell] = player_index;
+        // maintain/build the edges of the player graph after adding the cell
+        for (auto neighbor : all_neighbors(cell)) {
+          if ((get_player(neighbor) == player_index)) {
+            // has to be a symmetric edge
+            players_graphs[player_index]->add_symmetric_edge(
+                mylib::Edge<std::pair<std::size_t, std::size_t>>(neighbor, cell,
+                                                                 -1));
+          }
+        }
+        return true;
       }
-    return true;
     }
-  return false;
+    return false;
   }
+
+  /*
+   * A small sanity check of the grid params.
+   */
 
   bool check() {
     auto check = (board.size() == x_size);
--- tests/test_hex.hpp
+++ tests/test_hex.hpp
@@ -11,10 +11,21 @@ TEST(HexBoardTest, should_return_true_on_within_the_cell_board) {
   ASSERT_TRUE(hex_board.within_the_board(std::make_pair(1, 1)));
 }
 
+
 TEST(HexBoardTest, should_return_false_on_within_the_cell_board) {
   auto hex_board = mylib::HexBoard<int>(2, 2);
   ASSERT_FALSE(hex_board.within_the_board(std::make_pair(2, 1)));
 }
+
+
+TEST(HexBoardTest, should_treat_edges_cases_properly) {
+  auto hex_board = mylib::HexBoard<int>(3, 3);
+  ASSERT_EQ(hex_board.all_neighbors(std::make_pair(0, 0)).size(), 2);
+  ASSERT_EQ(hex_board.all_neighbors(std::make_pair(1, 1)).size(), 6);
+  ASSERT_EQ(hex_board.all_neighbors(std::make_pair(0, 2)).size(), 3);
+  ASSERT_EQ(hex_board.all_neighbors(std::make_pair(2, 2)).size(), 2);
+}
+
 
 TEST(HexBoardTest, should_build_graph_properly_for_the_first_player) {
   auto hex_board = mylib::HexBoard<int>(4, 4, 1); // builds one player config.
@@ -22,9 +33,72 @@ TEST(HexBoardTest, should_build_graph_properly_for_the_first_player) {
   ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(0, 1), 0));
   ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(0, 2), 0));
 
-  auto player_graph =  hex_board.get_player_graph(0);
-  ASSERT_EQ(player_graph.get().get_edges_number(), 2);
+  auto player_graph = hex_board.get_player_graph(0);
+  ASSERT_EQ(player_graph.get()->get_edges_number(), 4);
 
-  ASSERT_EQ(player_graph.get().get_path_length({std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2)}), 2);
-  ASSERT_EQ(player_graph.get().dijkstra(std::make_pair(0, 0), std::make_pair(0, 2)).size(), 3);
+  ASSERT_EQ(
+      player_graph.get()->get_path_length(
+          {std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2)}),
+      -2);
+  ASSERT_EQ(player_graph.get()->get_path_length(player_graph.get()->dijkstra(
+                std::make_pair(0, 0), std::make_pair(0, 2))),
+            -2);
+}
+
+
+/*
+* Should connect the graph on connecting cell assignment.
+* assigning the cell c to the player 0 (first player) should connect the player
+graph
+* Note that the grid is rotated (90 degree counter clock-wise) here for ascii
+ drawing convenience.
+                          _____
+                         /     \
+                   _____/       \
+                  /     \       /
+            _____/   0   \_____/
+           /     \       /     \
+     _____/       \_____/       \
+    /     \       /     \       /
+   /       \_____/   0   \_____/
+   \       /     \       /     \
+    \_____/   c   \_____/       \
+    /     \       /     \       /
+   /   0   \_____/       \_____/
+   \       /     \       /     \
+    \_____/       \_____/       \
+    /     \       /     \       /
+   /   0   \_____/       \_____/
+   \       /     \       /
+    \_____/       \_____/
+    /     \       /
+   /   0   \_____/
+   \       /
+    \_____/
+
+ */
+
+TEST(HexBoardTest, should_connect_the_graph_on_connecting_cell_assignment) {
+  auto hex_board = mylib::HexBoard<int>(4, 4, 1); // builds one player config.
+  // adding the first sub-graph to the player 0
+  ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(0, 0), 0));
+  ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(0, 1), 0));
+  ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(0, 2), 0));
+  // adding the a second sub-graph to the player 0
+  ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(2, 2), 0));
+  ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(2, 3), 0));
+
+  auto player_graph = hex_board.get_player_graph(0);
+
+  // assert that there is no path between cells (0,0) and (2, 3)
+  ASSERT_EQ(player_graph.get()
+                ->dijkstra(std::make_pair(0, 0), std::make_pair(2, 3))
+                .size(),
+            0);
+
+  // adding the cell c (1,2)
+  ASSERT_TRUE(hex_board.assign_cell_to_player(std::make_pair(1, 2), 0));
+  auto dijkstra_path =
+      player_graph.get()->dijkstra(std::make_pair(0, 0), std::make_pair(2, 3));
+  ASSERT_EQ(dijkstra_path.size()-1, 5); // edges count
 }
